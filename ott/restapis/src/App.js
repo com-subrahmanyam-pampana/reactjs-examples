7eadaeb77ce0c84a5f4cc226a6649c1487b66a26
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-11-23 00:26:14 +0530

    react step

---
+++ ott/restapis/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {  Routes, Route,Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
+
+
+function App() {
+  return (
+   <div>
+     {/* Navigation */}
+     <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/signin">Signin</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+     <Routes>
+     <Route  path="/" element={<SigninPage />} />
+        <Route  path="/home" element={<HomePage />} />
+        <Route  path="/signin" element={<SigninPage />} />
+        <Route  path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+    </Routes>
+   </div>
+  );
+}
+
+export default App;
